410b0caaa010a014c060752fd1ba7f39a3ee811d
da2my <da2my.icloud.com>
2020-08-11 20:29:52 +0200

    rellenalista() para introducir los salarios y numSalarios() para obtener el numero de empleados que cobra mas de 1000

--- src/ejerciciosEstructurasDeControl/SalarioMasAlto_27.java
+++ src/ejerciciosEstructurasDeControl/SalarioMasAlto_27.java
@@ -4,19 +4,29 @@ import vista.Consola;
 
 public class SalarioMasAlto_27 {
 
-	private int[] empleados = new int[5];
+	private int[] empleados = new int[6];
 
 	public SalarioMasAlto_27() {
 
 	}
 
-	public void pedirSalario() {
-
-		for (int i = 0; i < empleados.length; i++) {
+	public int[] rellenaLista() {
+		for (int i = 1; i < empleados.length; i++) {
 			empleados[i] = Consola.pedirNum();
 		}
+		return empleados;
 	}
-	
-	
 
+	public void numSalarios() {
+		int iNumeroMayor = 1000;
+		int contador = 0;
+		empleados = rellenaLista();
+
+		for (int i = 1; i < empleados.length; i++) {
+			if (empleados[i] > iNumeroMayor) {
+				contador++;
+			}
+		}
+		System.out.println("\n" + contador);
+	}
 }
