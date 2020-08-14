fe3100cbd3a75785978f48607e371de0c9e74ba0
da2my <da2my.icloud.com>
2020-08-14 21:28:41 +0200

    En este ejercicio he utilizado switch para saber cuantos suspensos, aprobados, notables y sobresalientes hay. Y en el caso default he discriminado las notas que se toman en cuenta y he utilizado el operador xor para que el -1 no cuente como incorrecto

--- src/ejerciciosEstructurasDeControl/Notas_29.java
+++ src/ejerciciosEstructurasDeControl/Notas_29.java
@@ -1,5 +1,56 @@
 package ejerciciosEstructurasDeControl;
 
+import vista.Consola;
+
 public class Notas_29 {
 
+	private int nota;
+	private int cSuspenso;
+	private int cAprobado;
+	private int cNotable;
+	private int cSobresaliente;
+
+	public Notas_29() {
+
+	}
+
+	public void evaluaNotas() {
+
+		System.out.println("Introducir notas");
+		do {
+			nota = Consola.pedirNum();
+
+			switch (nota) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				++cSuspenso;
+				break;
+			case 5:
+			case 6:
+				++cAprobado;
+				break;
+			case 7:
+			case 8:
+				++cNotable;
+				break;
+			case 9:
+			case 10:
+				++cSobresaliente;
+				break;
+
+			default:
+				if (!(nota > 0 && nota < 10) ^(nota == -1)) {
+					System.out.println("Nota incorrecta");
+				}
+					break;
+			}
+		} while (!(nota == -1));
+		System.out.println("Numero de notas suspensos: " + cSuspenso);
+		System.out.println("Numero de notas aprobadas: " + cAprobado);
+		System.out.println("Numero de notas notable: " + cNotable);
+		System.out.println("Numero de notas sobresaliente: " + cSobresaliente);
+
+	}
 }
