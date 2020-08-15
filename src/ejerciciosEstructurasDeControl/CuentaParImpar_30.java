fcb15cdf25f01ce245caf966454db23378b372b2
da2my <da2my.icloud.com>
2020-08-16 00:02:29 +0200

    Metodo que cuenta hasta 100 de forma aleatoria

---
+++ src/ejerciciosEstructurasDeControl/CuentaParImpar_30.java
@@ -0,0 +1,15 @@
+package ejerciciosEstructurasDeControl;
+
+public class CuentaParImpar_30 {
+
+//	private int [] numeros = new int [100];
+	private int num=101;
+	
+	public void recorreNum() {
+		for (int i = 0; i < num; i++) {
+			
+			System.out.println((int)Math.floor(Math.random()*100));
+			
+		}
+	}
+}
