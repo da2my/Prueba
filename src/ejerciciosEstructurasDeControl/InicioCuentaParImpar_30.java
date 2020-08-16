4a1c64b6301eac5ad8d0457c918c3285e2f6f57a
da2my <da2my.icloud.com>
2020-08-16 13:36:48 +0200

    Ejecuta ejercicio 30

---
+++ src/ejerciciosEstructurasDeControl/InicioCuentaParImpar_30.java
@@ -0,0 +1,11 @@
+package ejerciciosEstructurasDeControl;
+
+public class InicioCuentaParImpar_30 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new CuentaParImpar_30().recorreNum();
+//		System.out.println();
+	}
+
+}
