d330b45975a2acb4df67d2a1e47c6ae383a1d422
da2my <da2my.icloud.com>
2020-08-20 13:46:16 +0200

    ejercicio completado, repasar conceptos de como recorre un if a un array, de como no se puede asignar un array a una variable para que sea machacada porque en realidad estamos expuestos a cambiar las dimensiones del array, y ver el restador de posicion de el for. muy importante. Este ejercicio reune varios conceptos!!!

--- src/ejerciciosEstructurasDeControl/RangoNumeros_37.java
+++ src/ejerciciosEstructurasDeControl/RangoNumeros_37.java
@@ -4,28 +4,35 @@ import vista.Consola;
 
 public class RangoNumeros_37 {
 
-	
-	private int[] num = new int [11];
-	private int contNValidos=0;
+	private int[] num = new int[11];
+	private int contNValidos = 0;
+	private int numMayor = 0;
+	private int aux = 0;
 	Consola con = new Consola();
-	
+
 	public static void main(String[] args) {
 		new RangoNumeros_37().rango();
-		
+
 	}
-	
+
 	public void rango() {
-		
+
 		for (int i = 0; i < num.length; i++) {
-			num[i] = Integer.parseInt(con.introCadena("Introduce 11 numeros entre 20 - 40"));				
-			if (!(num[i]>=20 && num[i]<=40)) {
-				
-				contNValidos=i--;
+			num[i] = Integer.parseInt(con.introCadena("Introduce 11 numeros entre 20 - 40"));
+			aux = num[i];// aux es necesario para hacer que cuente el numero mayor de los validos
+			if (!(num[i] >= 20 && num[i] <= 40)) {
+				i--;// cuando el numero introducido no es valido, se resta una posicion, de "i" del
+					// contador del for. Y por lo tanto tambien se machaca el elemento del array
+				contNValidos++;
 				System.out.println("numero no permitido");
+			} else {
+				if (numMayor < aux) {
+					numMayor = aux;
+				}
 			}
 		}
-		System.out.println(contNValidos);
-		
+		System.out.println("\n" + "Has introducido " + contNValidos + " numeros no validos.");
+		System.out.println("\n" + "El mayor de los numeros validos es el: " + numMayor);
 	}
 
 }
