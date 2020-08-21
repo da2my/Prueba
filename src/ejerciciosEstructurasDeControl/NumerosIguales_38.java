d5a8c6708d31f501040cdb523ebc908fb30d2fa8
da2my <da2my.icloud.com>
2020-08-22 01:12:26 +0200

    he conseguido que funcione pero tengo un problema con el cero, cuando pongo dos ceros sucesivos no sale del bucle. REVISAR.

---
+++ src/ejerciciosEstructurasDeControl/NumerosIguales_38.java
@@ -0,0 +1,31 @@
+package ejerciciosEstructurasDeControl;
+
+import vista.Consola;
+
+public class NumerosIguales_38 {
+
+	private int num;
+	private int aux;
+	private int aux1;
+//	private boolean cond; 
+
+	Consola con = new Consola();
+
+	public static void main(String[] args) {
+		new NumerosIguales_38().introNumIguales();
+	}
+
+	public void introNumIguales() {
+		while ((num != aux1) || (num == 0)) {
+			num = Integer.parseInt(con.introCadena("Introduce numeros hasta que dos sean iguales y seguidos: "));
+			if ((aux == num)) {
+				aux1 = aux;
+			}
+			if (aux != num) {
+				aux = num;
+			}
+		}
+		System.out.println("Salio!!"+"\n"+"Se ha introducido dos numeros iguales");
+	}
+
+}
