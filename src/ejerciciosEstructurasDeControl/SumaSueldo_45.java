a54f1c063afe287c3b096791b9b3897338b6fbd9
da2my <da2my.icloud.com>
2020-09-02 00:16:15 +0200

    Con array dentro de un for por medio de un contador y una variable auxiliar que suma los sueldos con el operador +=

--- src/ejerciciosEstructurasDeControl/SumaSueldo_45.java
+++ src/ejerciciosEstructurasDeControl/SumaSueldo_45.java
@@ -4,40 +4,47 @@ import java.util.ArrayList;
 
 public class SumaSueldo_45 {
 
-//	private int[] sueldo = new int[10];
-	
+	private int[] sueldo = new int[10];
+
 	ArrayList<Integer> sueldos = new ArrayList();
-	private int sueldo;
+//	private int sueldo;
+	private int cont;
+	private int aux;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new SumaSueldo_45().pideSueldos();
+		new SumaSueldo_45().sumaSueldos();
 	}
 
-	public void pideSueldos() {
+	public void sumaSueldos() {
 //		for (int i : sueldo) {
-//			sueldo[i] = (int) (Math.random() * 100 * (Math.random() > 0.1 ? 1 : -1));
-//			System.out.println(sueldo[i]);
-//		}
-
-//		for (int i = 0; i < sueldo.length; i++) {
-//			
-//			sueldo[i] = (int) (Math.random()*(10000-950+1)+950);
+//			sueldo[i] = (int) (Math.random() * (5000 - 950 + 1) + 950);
 //
 //			System.out.println(sueldo[i]);
 //		}
-		
-		for (int i = 1; i <= 10; i++) {
-		    sueldo = (int) (Math.random() * 10000 + 950);
-		    if (sueldos.contains(sueldo)) {
-		        i--;
-		    } else {
-		        sueldos.add(sueldo);
-		    }
-		    System.out.println(sueldo);
+
+		for (int i = 0; i < sueldo.length; i++) {
+			
+			sueldo[i] = (int) (Math.random()*(2000-450+1)+550);
+			if (sueldo[i] > 1000) {
+				cont++;
+			}
+			aux+=sueldo[i];
+			System.out.println(sueldo[i]);
 		}
-		
-		
+		System.out.println("\n"+"La suma total de los sueldos es: "+aux);
+		System.out.println("\n"+"Hay "+cont+" sueldos "+" mayores de a 1000€");
+
+//		for (int i = 1; i <= 10; i++) {
+//		    sueldo = (int) (Math.random() * 10000 + 950);
+//		    if (sueldos.contains(sueldo)) {
+//		        i--;
+//		    } else {
+//		        sueldos.add(sueldo);
+//		    }
+//		    System.out.println(sueldo);
+//		}
+
 	}
 
 }
